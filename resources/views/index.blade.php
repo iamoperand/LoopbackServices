493263743b4650e8511ad0021f86514451fa87b9
= <=>
2016-10-04 16:18:52 +0530

    Nikhil

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,9 +3,9 @@
 
 	<body>
 		
-<<<<<<< HEAD
+
 	@include('partials._header')
-=======
+
 	<header id="genit-header" role="banner">
 		<nav class="navbar navbar-default" role="navigation">
 			<div class="container-fluid">
@@ -26,7 +26,7 @@
 		</nav>
 	</header>
 	<!-- END .header -->
->>>>>>> 1d4135f8314df5312bc1c7724f38f08f586ed68f
+
 	
 	<div class="genit-slider">
 		<div class="owl-carousel owl-carousel-fullwidth">
@@ -174,84 +174,9 @@
 
 	</div>
 
-<<<<<<< HEAD
+
 
 @include('partials._footer')
 	
-=======
-	<footer id="genit-footer">
-		
-		<div class="container">
-			<div class="row">
-				<div class="col-md-6 col-sm-6">
-					<div class="genit-footer-widget">
-						<h2 class="genit-footer-logo">Booster</h2>
-						<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-					</div>
-					<div class="genit-footer-widget">
-						<ul class="genit-social">
-							<li><a href="#"><i class="icon-facebook"></i></a></li>
-							<li><a href="#"><i class="icon-twitter"></i></a></li>
-							<li><a href="#"><i class="icon-instagram"></i></a></li>
-							<li><a href="#"><i class="icon-linkedin"></i></a></li>
-							<li><a href="#"><i class="icon-youtube"></i></a></li>
-						</ul>
-					</div>
-				</div>
-
-				<div class="col-md-2 col-sm-6">
-					<div class="genit-footer-widget top-level">
-						<h4 class="genit-footer-lead ">Company</h4>
-						<ul>
-							<li><a href="#">About</a></li>
-							<li><a href="#">Contact</a></li>
-						</ul>
-					</div>
-				</div>
-				
-				<div class="visible-sm-block clearfix"></div>
-
-				<div class="col-md-2 col-sm-12">
-					<div class="genit-footer-widget top-level">
-						<h4 class="genit-footer-lead">Service</h4>
-						<ul class="genit-list-check">
-							<li><a href="#">Network Monitoring</a></li>
-							<li><a href="#">Network Design & Optimization</a></li>
-							<li><a href="#">Network Security</a></li>
-							<li><a href="#">Disaster Recovery</a></li>
-							
-						</ul>
-					</div>
-				</div>
-				
-			</div>
-
-			<div class="row genit-row-padded genit-copyright">
-				<div class="col-md-5">
-					<p><small>&copy; 2016. All Rights Reserved. <br>Designed by <a href="http://genithub.com/" target="_blank">genIThub</a> </small></p>
-				</div>
-			</div>
-		</div>
-
-	</footer>
-
-
-	<!-- jQuery -->
-	<script src="js/jquery.min.js"></script>
-	<!-- jQuery Easing -->
-	<script src="js/jquery.easing.1.3.js"></script>
-	<!-- Bootstrap -->
-	<script src="js/bootstrap.min.js"></script>
-	<!-- Owl carousel -->
-	<script src="js/owl.carousel.min.js"></script>
-	<!-- Waypoints -->
-	<script src="js/jquery.waypoints.min.js"></script>
-	<!-- Magnific Popup -->
-	<script src="js/jquery.magnific-popup.min.js"></script>
-	<!-- Main JS -->
-	<script src="js/main.js"></script>
 
 	
-	</body>
-</html>
->>>>>>> 1d4135f8314df5312bc1c7724f38f08f586ed68f
